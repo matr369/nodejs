18293b41092f316d10a7964c1c793887a5b7cf74
Zhenya Ivashkevich <matr369@gmail.com>
2014-08-13 15:24:07 +0300

    fix

---
+++ exstserver/rest/Collections/technology.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 13.08.2014.
+ */
+var base = require("./Base");
+var $ = require("Deferred");
+var Model = require("./../Models/technology");
+module.exports = base.extend({
+    model: Model
+});
