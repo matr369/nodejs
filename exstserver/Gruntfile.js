b3e10c3a87a4682a7571b7ef2ea031ccfaac0eba
mantsevich <mantsevich.dzenis@gmail.com>
2014-08-13 15:24:18 +0300

    Add quick build

--- exstserver/Gruntfile.js
+++ exstserver/Gruntfile.js
@@ -3,33 +3,41 @@
  */
 module.exports = function(grunt) {
 
+    var SRC = [
+        "<%= jsRoot %>/libs/jquery-2.1.1.js",
+        "<%= jsRoot %>/libs/lodash-2.4.1.js",
+        "<%= jsRoot %>/libs/sha3.js",
+        "<%= jsRoot %>/libs/doT.js",
+        "<%= jsRoot %>/libs/backbone.js",
+        "<%= jsRoot %>/libs/bootstrap.min.js",
+        "<%= jsRoot %>/libs/typeahead.jquery.js",
+        "<%= jsRoot %>/Core/**/*.js",
+        "<%= jsRoot %>/Config.js",
+        "<%= jsRoot %>/App.js",
+        "<%= jsRoot %>/main.js"
+    ];
+    var resultFile = "<%= jsRoot %>/application.js",
+        resultMinFile = "<%= jsRoot %>/application.min.js";
+
     grunt.initConfig({
         jsRoot: "public/resources/js",
         pkg: grunt.file.readJSON('package.json'),
         concat: {
             options: {},
-            dist: {
-                src: [
-                    "<%= jsRoot %>/libs/jquery-2.1.1.js",
-                    "<%= jsRoot %>/libs/lodash-2.4.1.js",
-                    "<%= jsRoot %>/libs/sha3.js",
-                    "<%= jsRoot %>/libs/doT.js",
-                    "<%= jsRoot %>/libs/backbone.js",
-                    "<%= jsRoot %>/libs/bootstrap.min.js",
-                    "<%= jsRoot %>/libs/typeahead.jquery.js",
-                    "<%= jsRoot %>/Core/**/*.js",
-                    "<%= jsRoot %>/Config.js",
-                    "<%= jsRoot %>/App.js",
-                    "<%= jsRoot %>/main.js"
-                ],
-                dest: "<%= jsRoot %>/application.js"
+            app: {
+                src: SRC,
+                dest: resultFile
+            },
+            min: {
+                src: SRC,
+                dest: resultMinFile
             }
         },
         'closure-compiler': {
             frontend: {
                 closurePath: './',
-                js: "<%= jsRoot %>/application.js",
-                jsOutputFile: "<%= jsRoot %>/application.min.js",
+                js: resultFile,
+                jsOutputFile: resultMinFile,
                 maxBuffer: 500,
                 noreport: true,
                 options: {
@@ -47,6 +55,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-closure-compiler');
 
-    grunt.registerTask('default', ['concat', 'closure-compiler']);
+    grunt.registerTask('default', ['concat:app', 'closure-compiler']);
+    grunt.registerTask('quick', ['concat:min']);
 
 };
